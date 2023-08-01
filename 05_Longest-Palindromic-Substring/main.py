e512cabb3a65bb01f0e9e9af26fc7768b2fe2d11
Chin Pang <chinpang99@outlook.com>
2023-08-01 22:17:04 +0800

    Upload Leet Code Question 5

---
+++ 05_Longest-Palindromic-Substring/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        return 0
+    
+
+if __name__ == "__main__":
+    solution = Solution()
+    result = solution.longestPalindrome()
+    
